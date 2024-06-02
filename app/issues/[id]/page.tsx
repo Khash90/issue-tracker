fe1df8539b51da492320e527ee81b7be83efa271
Khash <90476560+Khash90@users.noreply.github.com>
2024-06-02 15:35:53 +0330

    added aditional loading skeletons

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,40 +1,36 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
-import prisma from '@/prisma/client'
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
-import delay from 'delay';
-import { notFound } from 'next/navigation';
-import ReactMarkdown from 'react-markdown'
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import prisma from "@/prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+
+import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
 
 interface Props {
-    params: { id: string }
+  params: { id: string };
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
+  // if(typeof params.id !== 'number')
+  //     notFound()
 
-    // if(typeof params.id !== 'number') 
-    //     notFound()
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
 
-    await delay(2000)
-
-   const issue = await prisma.issue.findUnique({
-    where: {id: parseInt(params.id)}
-   });
-
-   if(!issue) 
-    notFound();
+  if (!issue) notFound();
 
   return (
     <div>
-        <Heading>{issue.title}</Heading>
-        <Flex gap="3" className='items-center my-2'>
+      <Heading>{issue.title}</Heading>
+      <Flex gap="3" className="items-center my-2">
         <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card className='prose mt-6'>
+      </Flex>
+      <Card className="prose mt-6">
         <ReactMarkdown>{issue.description}</ReactMarkdown>
-        </Card>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default IssueDetailPage
+export default IssueDetailPage;
