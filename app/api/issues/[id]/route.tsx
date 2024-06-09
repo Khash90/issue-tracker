2ccefa90c42aea9bf5a74e4d7e9d797c39577a8a
Khash <90476560+Khash90@users.noreply.github.com>
2024-06-09 10:37:20 +0330

    Made DELETE API

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -12,27 +12,45 @@ export async function PATCH(
   request: NextResponse,
   { params }: { params: { id: string } }
 ) {
-   const body = await request.json();
-   const validation = issueSchema.safeParse(body)
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
 
-   if (!validation.success)
-    return NextResponse.json(validation.error.format(), {status: 400})
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
-   const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) }
-   });
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
 
-   if (!issue)
-    return NextResponse.json({ error: 'Invalid Issue' }, {status: 404})
+  if (!issue)
+    return NextResponse.json({ error: "Invalid Issue" }, { status: 404 });
 
-   const updatedIssue = await prisma.issue.update({
-    where: {id: issue.id},
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
     data: {
-        title: body.title,
-        description: body.description
-    }
-   })
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(
+  request: NextResponse,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  })
+
+  if (!issue)
+    return NextResponse.json({ error: "Invalid Issue" }, { status: 404 })
+
+    await prisma.issue.delete({
+    where: { id: issue.id }
+  });
 
 
-   return NextResponse.json(updatedIssue)
+  return NextResponse.json({})
 }
