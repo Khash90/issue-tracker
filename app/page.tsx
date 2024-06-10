a979d26464c2cc4b26ed53befee617846498ec44
Khash <90476560+Khash90@users.noreply.github.com>
2024-06-10 15:46:45 +0330

    Built the latest issues component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-import Image from "next/image";
+
+import LatestIssues from "./LatestIssues";
 
 export default function Home() {
   return (
-     <div>Hello Khash</div>
+     <LatestIssues />
   );
 }
