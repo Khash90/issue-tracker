663552cdc557761266f332eda9d77d5be70f7e9c
Khash <90476560+Khash90@users.noreply.github.com>
2024-06-11 12:39:09 +0330

    Built the layout of pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 
+import Pagination from "./components/Pagination";
 import LatestIssues from "./LatestIssues";
 
 export default function Home() {
   return (
-     <LatestIssues />
+    //  <LatestIssues />
+    <Pagination itemCount={100} pageSize={10} currentPage={10} />
   );
 }
